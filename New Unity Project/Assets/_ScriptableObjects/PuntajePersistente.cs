edc74333307ca25009ca5c1f4f247aa72f838a3d
KarimoSP <KarimSP_98@outlook.com>
2022-07-23 03:35:59 -0500

    Clase 13
    Se agregaron elementos UI

---
+++ New Unity Project/Assets/_ScriptableObjects/PuntajePersistente.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+
+//[CreateAssetMenu(fileName = "PuntajePersistente", menuName = "New Unity Project/PuntajePersistente", order = 0)]
+
+public abstract class PuntajePersistente : ScriptableObject 
+{
+    public void Guardar(string NombreArchivo = null)
+    {
+        var bf = new BinaryFormatter();
+        var file = File.Create(ObtenerRuta(NombreArchivo));
+        var json = JsonUtility.ToJson(this);
+
+        bf.Serialize(file, json);
+        file.Close();
+    }
+
+
+    public virtual void Cargar(string nombreArchivo = null)
+    {
+        if(File.Exists(ObtenerRuta(nombreArchivo)));
+        {
+            var bf = new BinaryFormatter();
+            var archivo = File.Open(ObtenerRuta(nombreArchivo), FileMode.Open);
+            JsonUtility.FromJsonOverwrite((string)bf.Deserialize(archivo),this);
+            archivo.Close();
+        }
+    }
+
+    public string ObtenerRuta(string nombreArchivo = null)
+    {
+        var nombreArchivoCompleto = string.IsNullOrEmpty(nombreArchivo) ? name : nombreArchivo;
+        return string.Format("{0}/{1}.kioan", Application.persistentDataPath, nombreArchivoCompleto);
+    }
+
+
+}
+
---
+++ New Unity Project/Assets/_Scripts/Puntaje.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Puntaje : MonoBehaviour
+{
+    public Transform transformPuntajeAlto;
+    public Transform transformPuntajeActual;
+    public TMP_Text textoPuntajeAlto;
+    public TMP_Text textoActual;
+    public PuntajeAlto puntajeAltoSO;
+    // public int puntos = 0;
+    // public int puntuajeAlto = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transformPuntajeActual = GameObject.Find("PuntuajeActual").transform;
+        transformPuntajeAlto = GameObject.Find("PuntuajeAlto").transform;
+        textoActual = transformPuntajeActual.GetComponent<TMP_Text>();
+        textoPuntajeAlto = transformPuntajeAlto.GetComponent<TMP_Text>();
+        //if (PlayerPrefs.HasKey("PuntuajeAlto")) //Manera antigua de sistema de guardado
+        //{
+            // puntuajeAlto = PlayerPrefs.GetInt("PuntuajeAlto");
+        //}
+        puntajeAltoSO.Cargar();
+        textoPuntajeAlto.text = $"PuntajeAlto: {puntajeAltoSO.puntajeAlto}";
+        puntajeAltoSO.puntaje = 0;
+    }
+
+    private void FixedUpdate() 
+    {
+        puntajeAltoSO.puntaje += 50;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        textoActual.text = $"PuntuajeActual: {puntajeAltoSO.puntaje}";
+        if(puntajeAltoSO.puntaje > puntajeAltoSO.puntajeAlto)
+        {
+            puntajeAltoSO.puntajeAlto = puntajeAltoSO.puntaje;
+            textoPuntajeAlto.text =  $"PuntuajeAlto: {puntajeAltoSO.puntajeAlto}";
+        }
+        // textoActual.text = $"PuntuajeActual: {puntos}";
+        // if(puntos > puntuajeAlto)
+        // {
+        //     puntuajeAlto = puntos;
+        //     textoPuntajeAlto.text =  $"PuntuajeActual: {puntos}";
+        //     PlayerPrefs.SetInt("PuntuajeAlto", puntos); //Manera antigua de sistema de guardado
+        // }
+    }
+}
